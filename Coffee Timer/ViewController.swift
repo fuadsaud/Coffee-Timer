d20509f7ed8e69890c012d467d37b4c3238f0646
Fuad Saud <fuadfsaud@gmail.com>
2015-10-31 23:04:00 -0200

    This is a commit

--- Coffee Timer/ViewController.swift
+++ Coffee Timer/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        println("View is loaded")
+        view.backgroundColor = UIColor.orangeColor()
     }
 
     override func didReceiveMemoryWarning() {
